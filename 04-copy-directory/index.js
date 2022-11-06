64bd8413b1b0a4f745ac059647fbf47ef5968e65
vlxdxrtxmxv <vladhort3@gmail.com>
2022-11-06 14:45:21 +0300

    Add some code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,55 @@
+const fs = require('fs');
+const path = require('path');
 
+let filename = __dirname + '\\files';
+let filenameCopy = __dirname + '\\files-copy';
+let dirCur = path.join(filename);
+let dirCopy = path.join(filenameCopy);
+
+function createDir(dirCopy) {
+
+    fs.access(dirCopy, (error) => {
+        if (error && error.code === 'ENOENT') {
+            fs.mkdir(dirCopy, {recursive: true}, (error) => {
+                if (error) {
+                    return console.log(error.message);
+                }
+            });
+            copyDir(dirCur, dirCopy)
+        } else {
+            fs.readdir(dirCopy, (error, fileNames) => {
+                if (error) {
+                    return console.log(error.message);
+                }
+                fileNames.forEach(filename => {
+                    fs.unlink(path.join(dirCopy, filename), (error) => {
+                        if (error) {
+                            return console.log(error.message);
+                        }
+                    })
+                })
+            })
+            copyDir(dirCur, dirCopy)
+        }
+    });
+}
+
+function copyDir(dirCur, dirCopy) {
+    fs.readdir(dirCur, {withFileTypes: true}, (error, fileBuffer) => {
+
+        fileBuffer.forEach(filename => {
+
+            let src = path.join(dirCur, filename.name);
+            let copy = path.join(dirCopy, filename.name);
+
+                fs.copyFile(src, copy, function(error) {
+                    if (error) {
+                       return console.log(error.message);
+                    }
+                });
+        })
+    })
+}
+
+createDir(dirCopy);
+console.log('Directory created successfully!');
